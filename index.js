ae58ce53293fc9877f53492d1a3e961f4e472528
Florent Jaby <florent.jaby@gmail.com>
2013-03-27 16:32:47 +0100

    use stream-stream and be much cleaner

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var util = require('util');
 var fs = require('fs');
 var stream = require('stream');
 var split = require('split');
+var SS = require('stream-stream');
+
 
 function CatStream (encoding, transformer, options) {
     if(!(this instanceof CatStream)) return new CatStream(encoding, transformer, options);
@@ -36,37 +38,47 @@ function CatStream (encoding, transformer, options) {
         }
     }
 
-    stream.Transform.call(this, this._options);
+    stream.Duplex.call(this, this._options);
     if(this._options) this._separator = this._options.separator;
-    this._queue = [];
     this._split = split(this._separator);
     this._split.on('data', this._onFilename.bind(this));
-}
-util.inherits(CatStream, stream.Transform);
+    this._ss = SS();
 
-CatStream.prototype._transform = function _transform(chunk, encoding, callback) {
+    this._split.on('end', this._ss.end.bind(this._ss));
+
+    this._ss.on('end', function() {
+        this._ended = true;
+    }.bind(this));
+
+    this._ss.on('readable', this.read.bind(this, 0));
+}
+util.inherits(CatStream, stream.Duplex);
+
+CatStream.prototype._write = function _write(chunk, encoding, callback) {
     this._split.write(chunk, encoding);
     callback();
 };
 
-CatStream.prototype._flush = function _flush(callback) {
-    var self = this;
-    this._split.end();
-    if(this._queue.length) {
-        this._queue[this._queue.length-1].on('end', function(err, res) {
-            self.push(null);
-            callback();
-        });
-        this._startHead()
+CatStream.prototype._read = function _read(size) {
+    var res = this._ss.read(size);
+    if(res === null && this._ended) {
+        this.push(null);
     }
     else {
-        this.push(null)
+        this.push(res || '');
     }
 };
 
+CatStream.prototype.end = function end(chunk, encoding, callback) {
+    if(chunk) {
+        this._split.write(chunk, encoding, callback);
+    }
+    this._split.end();
+};
 
 CatStream.prototype._onFilename = function _onFilename(filename) {
-    if(filename.trim() == '') return;
+    filename = filename.trim();
+    if(filename == '') return;
 
     var self = this;
     var stream = fs.createReadStream(filename);
@@ -74,29 +86,7 @@ CatStream.prototype._onFilename = function _onFilename(filename) {
         stream = stream.pipe(this._transformer(filename));
     }
 
-    stream.pause();
-    stream.on('data', function(data) {
-        self.push(data);
-    });
-    stream.on('end', function() {
-        self._disposeHead();
-    });
-    self._queue.push(stream);
-    this._startHead();
-};
-
-CatStream.prototype._disposeHead = function _disposeHead() {
-    this._queue.shift();
-    this._startHead();
-};
-
-CatStream.prototype._startHead = function _startHead() {
-    if(this._queue.length) {
-        this._queue[0].resume();
-    }
-    else if(this._writableState.ended) {
-        this.push(null);
-    }
+    this._ss.write(stream);
 };
 
 module.exports = CatStream;
